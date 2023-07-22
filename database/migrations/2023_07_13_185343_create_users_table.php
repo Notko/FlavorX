01354204bfe08c5dcbb55716e319d275992affbd
= <=>
2023-07-22 20:10:34 +0200

    camelCase to snake_case

--- database/migrations/2023_07_13_185343_create_users_table.php
+++ database/migrations/2023_07_13_185343_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('pictureUri');
-            $table->string('firstName');
-            $table->string('lastName');
+            $table->string('picture_uri');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email');
             $table->string('password');
             $table->timestamps();
