a519f16330b1b25a61590a660462be6fd0927316
= <=>
2023-07-23 13:59:15 +0200

    Improve add method / Add login method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('api');
+    }
+
     /**
      * Add new user
      *
@@ -16,7 +22,7 @@ class UserController extends Controller
      */
     public function add(Request $request)
     {
-        $data = $request->validate(
+        $request->validate(
             [
                 'first_name' => 'required|between:2,32',
                 'last_name' => 'required|between:2,32',
@@ -37,24 +43,17 @@ class UserController extends Controller
             ]
         );
 
-        if ($errors = $data->errors()) {
-            return response()->json($errors, 422);
-        }
+        $user = User::create(
+            [
+                'picture_uri' => url('https://via.placeholder.com/128x128.png/4287f5?text=' . $request->first_name),
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]
+        );
 
-        $data['picture_uri'] = url('https://via.placeholder.com/128x128.png/4287f5?text=' . $data['first_name']);
-
-        $data['password'] = Hash::make($data['password']);
-
-        $users = User::create($data);
-
-        if ($users) {
-            return response()->json(
-                [
-                    'message' => 'User added successfully!'
-                ],
-                201,
-            );
-        } else {
+        if (!$user) {
             return response()->json(
                 [
                     'message' => 'Server Error'
@@ -62,5 +61,59 @@ class UserController extends Controller
                 500
             );
         }
+
+        $token = Auth::login($user);
+
+        return response()->json(
+            [
+                'message' => 'User added successfully!',
+                'user' => $user,
+                'authorization' => [
+                    'token' => $token,
+                    'type' => 'Bearer',
+                ]
+            ],
+            201,
+        );
+    }
+
+    /**
+     * Login user and return JWT
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $request->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ],
+            [
+                'email.required' => 'E-mail is required',
+                'email.email' => 'E-mail is invalid',
+                'password.required' => 'Password is required',
+            ]
+        );
+
+        $token = Auth::attempt($request->only('email', 'password'));
+
+        if (!$token) {
+            return response()->json([
+                'message' => 'Provided email and password do not match'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'Bearer',
+            ]
+        ], 200);
     }
 }
