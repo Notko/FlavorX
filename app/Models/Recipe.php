19eb2a49a5f72eaeee15a4475dbc7843cc131039
= <=>
2023-07-26 19:43:19 +0200

    Add fillable field

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Recipe extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'ingredients',
+        'instructions',
+        'user_id',
+    ];
 }
