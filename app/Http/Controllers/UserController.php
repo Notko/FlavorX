8eb1432e45f06062e0ef2c28fcc11d843b327b87
= <=>
2023-07-26 18:36:57 +0200

    Add user delete method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -188,4 +188,33 @@ class UserController extends Controller
             'message' => 'User updated successfully',
         ], 200);
     }
+
+    /**
+     * Delete user by id
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function delete(Request $request)
+    {
+        $userId = $request->id;
+        if (Auth::id() !== $userId) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 403);
+        }
+
+        $user = User::find($userId);
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        $user->delete();
+
+        return response()->json([
+            'message' => 'User deleted successfully',
+        ], 200);
+    }
 }
