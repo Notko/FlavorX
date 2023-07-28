b05447f6fcc6ce8fdd17f1857b2012735de20058
= <=>
2023-07-28 19:36:11 +0200

    Add add like method

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
+use App\Models\Recipe;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('api');
+    }
+
+    /**
+     * Like recipe by given recipe id
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function add(Request $request){
+        $request->validate([
+            'recipe_id' => 'required|numeric'
+        ], [
+            'recipe_id.required' => 'Recipe ID is required',
+            'recipe_id.numeric' => 'Recipe ID has to be numeric',
+        ]);
+
+        $recipeId = $request->recipe_id;
+        $userId = Auth::id();
+
+        $existingLike = Like::where('user_id', $userId)->where('recipe_id', $recipeId)->first();
+        if ($existingLike) {
+            return response()->json([
+                'message' => 'Recipe already liked',
+            ], 409);
+        }
+
+        $user = User::find($userId);
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        $recipe = Recipe::find($recipeId);
+        if (!$recipe) {
+            return response()->json([
+                'message' => 'Recipe not found'
+            ], 404);
+        }
+
+        $like = Like::create([
+            'user_id' => $userId,
+            'recipe_id' => $recipeId
+        ]);
+
+        if (!$like) {
+            return response()->json(
+                [
+                    'message' => 'Server Error'
+                ],
+                500
+            );
+        } else {
+            return response()->json(
+                [
+                    'message' => 'Liked successfully'
+                ],
+                201
+            );
+        }
+    }
 }
