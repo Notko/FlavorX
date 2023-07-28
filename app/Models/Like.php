51edb0293f844925f3a4c23d5d20aa176b3336de
= <=>
2023-07-28 19:36:52 +0200

    Add fillable

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Like extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'user_id',
+        'recipe_id',
+    ];
 }
